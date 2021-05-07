486fdbc2da8ad28e2c875a3bcacd71ef2f61f517
fenago <socrates73@gmail.com>
2021-05-07 08:00:31 +0500

    lab files 8-11 removed

--- Lab10/data/load_sensor_data.sh
+++
@@ -1,5 +0,0 @@
-for WORD in `cat sensor_data.json`
-do
-   echo $WORD
-   curl -XPOST -u sensor_data:sensor_data --header "Content-Type: application/json" "http://localhost:8080/" -d ''$WORD''
-done
